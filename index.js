4feab47f4a423205b2521898f937204a6532206d
Harsh Agarwal <harsh29092000@gmail.com>
2019-06-26 19:26:28 +0530

    Switched to discord.js commando

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
+/* eslint-disable brace-style */
 // require the discord.js module
-const Discord = require('discord.js');
+// const Discord = require("discord.js");
+const { CommandoClient } = require("discord.js-commando");
+const path = require("path");
 
 // config file required to run the bot
-const config = require('./config.json');
+const { prefix, token } = require("./config.json");
 
 // create a new Discord client
-const client = new Discord.Client();
+const client = new CommandoClient({
+	commandPrefix: prefix,
+	owner: 362645647545073685,
+	disableEveryone: true,
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		["first", "Your First Command Group"],
+		["second", "Your Second Command Group"],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, "commands"));
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity("Pokemon Go");
 });
 
+client.on("error", console.error());
+
+// client.on("message", message =>{
+// 	if (message.content.startsWith(`${prefix}ping`)) {
+// 		message.channel.send("Pong.");
+// 	} else if (message.content.startsWith(`${prefix}beep`)) {
+// 		message.channel.send("Boop.");
+// 	}
+// });
+
 // login to Discord with your app's token
-client.login(config.token);
+client.login(token);
