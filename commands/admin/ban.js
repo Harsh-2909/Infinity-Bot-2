371a9b79484b99e66672455e7e3f9718573354dc
Harsh Agarwal <harsh29092000@gmail.com>
2019-06-28 18:10:17 +0530

    Added Kick and Ban commands

---
+++ commands/admin/ban.js
@@ -0,0 +1,65 @@
+const {	Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+// const { RichEmbed } = require('discord.js');
+
+module.exports = class BanCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ban',
+			group: 'admin',
+			memberName: 'ban',
+			description: 'Bans out a user.',
+			guildOnly: true,
+			throttling: {
+				usages: 1,
+				duration: 30,
+			},
+			clientPermissions: ['BAN_MEMBERS'],
+			userPermissions: ['BAN_MEMBERS'],
+			args: [
+				{
+					key: 'User',
+					prompt: 'Who would you like to ban in the server?',
+					type: 'user',
+				},
+				{
+					key: 'reason',
+					prompt: 'Please provide a reason to ban?',
+					type: 'string',
+				},
+			],
+		});
+	}
+	run(message, { User, reason }) {
+		const bUser = message.guild.member(User);
+		if (!bUser) { return message.say('Couldn\'t find user.'); }
+		if(bUser.hasPermission('BAN_MEMBERS')) { return message.say('That person can\'t be banned.'); }
+
+		const bannedEmbed = new RichEmbed()
+			.setAuthor('Ban')
+			.setColor('157157')
+			.addField('Banned User', bUser)
+			.addField('Banned User ID', bUser.id)
+			.addField('Banned By', message.author)
+			.addField('Moderator ID', message.author.id)
+			.addField('Channel', message.channel)
+			.addField('Time', message.createdAt)
+			.addField('Reason', reason);
+
+		bUser.ban(reason);
+		bUser.send(`You have been banned in **${message.guild.name}** by **${message.author.tag}**.
+**Reason:** ${reason}`);
+		const bannedChannel = message.guild.channels.find('name', 'logs');
+		if(!bannedChannel) {
+			message.say('Couldn\'t find the appropriate channel. Please make one to store the reports.').then(msg => msg.delete(5000));
+			message.say(bannedEmbed);
+		}
+		else{
+			bannedChannel.send(bannedEmbed);
+		}
+
+		message.say(`${bUser.user.tag} banned successfully.`);
+		return message.delete().catch(O_o=>{ console.log(O_o); });
+	}
+
+};
---
+++ commands/admin/kick.js
@@ -0,0 +1,65 @@
+const {	Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+// const { RichEmbed } = require('discord.js');
+
+module.exports = class KickCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kick',
+			group: 'admin',
+			memberName: 'kick',
+			description: 'Kicks out a user.',
+			guildOnly: true,
+			throttling: {
+				usages: 1,
+				duration: 30,
+			},
+			clientPermissions: ['KICK_MEMBERS'],
+			userPermissions: ['KICK_MEMBERS'],
+			args: [
+				{
+					key: 'User',
+					prompt: 'Who would you like to kick out the server?',
+					type: 'user',
+				},
+				{
+					key: 'reason',
+					prompt: 'Please provide a reason to kick?',
+					type: 'string',
+				},
+			],
+		});
+	}
+	run(message, { User, reason }) {
+		const kUser = message.guild.member(User);
+		if (!kUser) { return message.say('Couldn\'t find user.'); }
+		if(kUser.hasPermission('KICK_MEMBERS')) { return message.say('That person can\'t be kicked.'); }
+
+		const kickedEmbed = new RichEmbed()
+			.setAuthor('Kick')
+			.setColor('157157')
+			.addField('Kicked User', kUser)
+			.addField('Kicked User ID', kUser.id)
+			.addField('Kicked By', message.author)
+			.addField('Moderator ID', message.author.id)
+			.addField('Channel', message.channel)
+			.addField('Time', message.createdAt)
+			.addField('Reason', reason);
+
+		kUser.kick(reason);
+		kUser.send(`You have been kicked out of **${message.guild.name}** by **${message.author.tag}**.
+**Reason:** ${reason}`);
+		const kickedChannel = message.guild.channels.find('name', 'logs');
+		if(!kickedChannel) {
+			message.say('Couldn\'t find the appropriate channel. Please make one to store the reports.').then(msg => msg.delete(5000));
+			message.say(kickedEmbed);
+		}
+		else{
+			kickedChannel.send(kickedEmbed);
+		}
+
+		message.say(`${kUser.user.tag} kicked successfully.`);
+		return message.delete().catch(O_o=>{ console.log(O_o); });
+	}
+
+};
